45d1bb61ec8374b7ef66604c9d5d21c8b40d1a26
Holiday <thelastofinusa@gmail.com>
2025-12-12 19:08:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
 
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://spawkdapp.vercel.app"
+            href="https://spawndapp.vercel.app"
             target="_blank"
             rel="noopener noreferrer"
           >
